5dd194e832b4838c60b71c0b881b5150e032a319
Sabsowitz <878789@cognizant.com>
2024-06-22 01:58:08 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CardModule, GridModule } from '@coreui/angular';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CardModule, GridModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
