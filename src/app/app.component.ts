2c248b49c5ef4ed65caf2f2d0e9c6cc40f451d60
Stephen Sabsowitz <prometheus1987@protonmail.com>
2024-12-18 00:39:54 -0500

    angular19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { CardModule, GridModule } from '@coreui/angular';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,CardModule, GridModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portal';
